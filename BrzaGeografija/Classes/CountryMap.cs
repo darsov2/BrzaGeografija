1617fce74eaee3e9f0381f7f0514da1479acb084
ninaak7 <ninakraleva01012002@gmail.com>
2023-07-06 23:36:58 +0200

    Add CountryMap class

---
+++ BrzaGeografija/Classes/CountryMap.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrzaGeografija.Classes
+{
+    internal class CountryMap
+    {
+        public string Country { get; set; }
+        public string Png { get; set; }
+    }
+}
